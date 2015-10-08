d6ea14735068fb1f50416749acc169ffa3c85074
suz <suziewithz@gmail.com>
2015-10-09 00:57:34 +0900

    Initial Commit

---
+++ waterful/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  waterful
+//
+//  Created by suz on 10/9/15.
+//  Copyright © 2015 suz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
