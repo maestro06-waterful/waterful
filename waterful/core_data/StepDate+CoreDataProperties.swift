d9210184f639589df053f8cc9ed4c9bbddb53661
JeongMinCha <cjm9236@naver.com>
2015-10-25 16:38:51 +0900

    Adding CoreData framework and arranging resources
    1. CoreData framework 추가
    CoreData framework를 사용할 수 있도록 CoreData DataModel 파일을
    추가하였고, delegate method 및 변수들을 추가함.
    그 안에 필요한 entity들도 정리하여 추가하였음.
    
    - StepDate: step count가 찍힌 date에 대한 로그
    - Setting: 세팅 값을 저장하는 엔티티, 사용자에 대한 정보도 포함됨
    - Plant: 식물들에 대한 정보를 저장함. 각 식물들은 객체처럼 취급될 수
      있어서 그를 구분하기 위해서 Id attribute도 추가하였음.
    - WaterLog: 물을 마신 로그를 표현. 이 역시 각 로그가 객체처럼 취급될 수
      있으므로 그를 위해 Id attribute를 추가하였음.
    
    2. 소스 및 자원 파일들 정리
    - 이미지 자원 파일들을 Assets.xcassets 내에서 관리할 수 있도록 위치
    변경하여 정리하였음. 또한, 이미지들을 images로 넣어서 파일 정리도
    하였음.
    - ViewController를 Grouping하였음. 가능하면 앞으로 소스 파일이 추가될
    때마다 유의미한 묶음(Grouping)을 해주었으면 좋겠음.
    
    3. app identifier 를 변경함
    suz.waterful이던 것을 com.waterful로 변경함.

---
+++ waterful/core_data/StepDate+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  StepDate+CoreDataProperties.swift
+//  waterful
+//
+//  Created by 차정민 on 2015. 10. 25..
+//  Copyright © 2015년 suz. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension StepDate {
+
+    @NSManaged var date: NSDate?
+
+}
