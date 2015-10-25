1cae1d46f667902d118b4f465b2885f5cbd42349
JeongMinCha <cjm9236@naver.com>
2015-10-25 20:17:50 +0900

    Adding routine to catch wakeup and fire noti
    HealthManager 클래스에서 wakeup을 캐치하고 로컬 푸쉬를 띄울 수 있도록
    구현하였음.
    
    스텝 카운트가 찍히는 매 한시간마다 observer query의 updateHandler가
    호출된다. 호출될 때마다 그 시간을 CoreData에 저장하는데, 그리고 저장된
    시간 로그들 중 4시간 이상 스텝을 찍지 않은 구간이 있었고, 그 직후에
    호출된 경우에 대해서는 로컬 푸쉬를 띄울 수 있도록 로직을 구현하였다.
    
    executeStepObserverQuery메서드에서 obsever query를 정의하고,
    백그라운드에서 헬스킷의 변화를 감지할 수 있도로 설정해두었고,
    saveCurrentDate 메서드에서 현재 시간을 CoreData에 저장한다.
    catchWakeup 메서드는 CoreData에 저장된 시간 로그들을 분석하여 4시간 이상
    스텝을 찍지 않은 구간 직후에 호출된 경우, 로컬 푸쉬를 띄우는 역할을
    한다.

--- waterful/HealthManager.swift
+++ waterful/HealthManager.swift
@@ -9,9 +9,13 @@
 import UIKit
 import Foundation
 import HealthKit
+import CoreData
 
 class HealthManager {
    
+    var appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    
     // Returns the Singleton instance
     class var sharedInstance: HealthManager {
         struct Singleton {
@@ -19,6 +23,9 @@ class HealthManager {
         }
         return Singleton.instance
     }
+    
+    // Sleep time
+    let sleepTime: NSTimeInterval = 4 * 3_600;  // 4 hours
     
     // healthkit store
     let healthKitStore: HKHealthStore = HKHealthStore()
@@ -46,6 +53,7 @@ class HealthManager {
             completion: {(success: Bool, error: NSError?) -> Void in
                 if success == true && error == nil {
                     print("Request Authorization succeeded.")
+                    self.executeStepObserverQuery()
                 } else {
                     print("Request Authorization failed.")
                 }
@@ -55,5 +63,127 @@ class HealthManager {
                 }
             }
         )
+    }
+    
+    func executeStepObserverQuery () {
+        
+        // Set the observer query for observing step counts
+        let stepObserverQuery: HKObserverQuery = HKObserverQuery(sampleType: stepCountType!, predicate: nil) {
+            (query, completionHandler, error) -> Void in    // updateHandler
+            print("updateHandler called. (step count)")
+            
+            // When updateHandler is called in the background mode
+            if self.appDelegate.isBackground == true {
+                print("background updateHandler called (step count)")
+                
+                // Save the current time to coreData framework.
+                // The date logs will be used to catch wakeup time
+                self.saveCurrentDate()
+                self.catchWakeup()
+                print("success!")
+            }
+            completionHandler()
+        }
+        
+        // Execute the observer query which is observing step count samples
+        self.healthKitStore.executeQuery(stepObserverQuery)
+        
+        // Enable the background delivery for step count samples
+        self.healthKitStore.enableBackgroundDeliveryForType(stepCountType!,
+            frequency: .Hourly) {
+                (success, error) -> Void in // completionHandler
+                
+                if success {
+                    print("Enabling background delivery for step count succeeded.")
+                } else {
+                    print("Enabling background delivery for step count failed.")
+                    if error != nil {
+                        print("error: \(error?.localizedDescription)")
+                    }
+                }
+        }
+    }
+    
+    // save current time (NSDate instance) to CoreData Framework
+    func saveCurrentDate() {
+        
+        let entityDescription = NSEntityDescription.entityForName("StepDate",
+            inManagedObjectContext: managedObjectContext)
+        let stepDate = StepDate(entity: entityDescription!,
+            insertIntoManagedObjectContext: managedObjectContext)
+        stepDate.date = NSDate()    // NSDate instance representing now
+        
+        do {
+            try managedObjectContext.save()
+            print("core data save() succeeded.")
+        } catch {
+            print("core data save() error")
+        }
+    }
+    
+    // This method is called when the updateHandler of HKObserverQuery observing step counts
+    // is called in the background modes.
+    func catchWakeup() {
+        
+        // Get step date logs in one day
+        let objects: [AnyObject]? = getStepDateLogsToday()
+        
+        if let results = objects {
+            if results.count > 1 {
+                var latestWakeupIndex: Int = 0
+                
+                // Calculate all time intervals and get max time interval
+                for index in 0...(results.count-2) {
+                    let date1 = (results[index+1] as! NSManagedObject).valueForKey("date")!
+                    let date2 = (results[index] as! NSManagedObject).valueForKey("date")!
+                    let timeInterval = date1.timeIntervalSinceDate(date2 as! NSDate)
+                    
+                    if timeInterval > sleepTime {
+                        latestWakeupIndex = index + 1
+                    }
+                }
+                
+                // There's no time interval larger than sleep time
+                if latestWakeupIndex == 0 {
+                    print("There was not a sleep time")
+                    return
+                }
+                
+                // latest wakeup log is now
+                if latestWakeupIndex == (results.count-1) {
+                    print("Wake up...!")
+                    appDelegate.registerNotification(NSDate().dateByAddingTimeInterval(60),
+                        alertBody: "방금 일어나셨군요! 하루를 알차게 잘 보내시기 바랍니다.")
+                } else {
+                    print("It passed an hour after wake up")
+                }
+            }
+        }
+    }
+    
+    // Returns date logs in the latest 24 hours
+    func getStepDateLogsToday() -> [AnyObject]? {
+        
+        // Set one day to 86,400 seconds
+        let day: Double = 86_400
+        
+        // "StepDate" entity in core data model
+        let entityDescription = NSEntityDescription.entityForName("StepDate", inManagedObjectContext: managedObjectContext)
+        let request = NSFetchRequest()
+        
+        // predicate = from '-one day' to 'now'
+        let pred = NSPredicate(format: "(date > %@)", NSDate().dateByAddingTimeInterval(-day))
+        request.entity = entityDescription
+        request.predicate = pred
+        
+        // Query results to return
+        var results: [AnyObject]? = nil
+        do {
+            results = try managedObjectContext.executeFetchRequest(request)
+        } catch {
+            print("Fetch Request Error.")
+            return nil
+        }
+        return results
     }
 }
