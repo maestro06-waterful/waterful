33f790f6ad11938489a3afbd1d3fb15d5e355697
yoon seok hong <yoonseok417@gmail.com>
2015-10-27 00:59:01 +0900

    Add notification manager

---
+++ waterful/WFNotification.swift
@@ -0,0 +1,9 @@
+//
+//  WFNotification.swift
+//  waterful
+//
+//  Created by HONGYOONSEOK on 2015. 10. 26..
+//  Copyright © 2015년 suz. All rights reserved.
+//
+
+import Foundation
---
+++ waterful/WFNotificationManager.swift
@@ -0,0 +1,9 @@
+//
+//  WFNotificationManager.swift
+//  waterful
+//
+//  Created by HONGYOONSEOK on 2015. 10. 26..
+//  Copyright © 2015년 suz. All rights reserved.
+//
+
+import Foundation
