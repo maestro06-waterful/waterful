fd827a4961b3fabcfafe5a48b35a99591420aece
yoon seok hong <yoonseok417@gmail.com>
2015-11-14 16:52:54 +0900

    remove Noti test

--- waterful/AppDelegate.swift
+++ waterful/AppDelegate.swift
@@ -54,7 +54,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             , categories: nil)
         app.registerUserNotificationSettings(notificationSettings)
         
-        NotiManager.sharedInstance?.registerSmartNoti(NotiManager.SmartNotiType.MORNING, fireDate: NSDate().dateByAddingTimeInterval(5))
         
         return true
     }
